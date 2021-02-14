c613375dbc6cae9b10c0cba14ff34d5bce2a325e
0x416c616e <29168822+0x416c616e@users.noreply.github.com>
2021-02-14 10:04:35 -0600

    implemented press command i.e. press enter, press a, press 2, press A, press b, press up, press escape, etc

--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,7 @@ import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.Optional;
 import java.util.Set;
@@ -57,6 +58,349 @@ public class Main extends Application {
         bot.mouseMove(x_end, y_end);
         bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 
+    }
+
+    public static void press(String key, Robot bot) {
+        switch (key) {
+            case "1":
+                bot.keyPress(KeyEvent.VK_1);
+                bot.keyRelease(KeyEvent.VK_1);
+                break;
+            case "2":
+                bot.keyPress(KeyEvent.VK_2);
+                bot.keyRelease(KeyEvent.VK_2);
+                break;
+            case "3":
+                bot.keyPress(KeyEvent.VK_3);
+                bot.keyRelease(KeyEvent.VK_3);
+                break;
+            case "4":
+                bot.keyPress(KeyEvent.VK_4);
+                bot.keyRelease(KeyEvent.VK_4);
+                break;
+            case "5":
+                bot.keyPress(KeyEvent.VK_5);
+                bot.keyRelease(KeyEvent.VK_5);
+                break;
+            case "6":
+                bot.keyPress(KeyEvent.VK_6);
+                bot.keyRelease(KeyEvent.VK_6);
+                break;
+            case "7":
+                bot.keyPress(KeyEvent.VK_7);
+                bot.keyRelease(KeyEvent.VK_7);
+                break;
+            case "8":
+                bot.keyPress(KeyEvent.VK_8);
+                bot.keyRelease(KeyEvent.VK_8);
+                break;
+            case "9":
+                bot.keyPress(KeyEvent.VK_9);
+                bot.keyRelease(KeyEvent.VK_9);
+                break;
+            case "0":
+                bot.keyPress(KeyEvent.VK_0);
+                bot.keyRelease(KeyEvent.VK_0);
+                break;
+            case "a":
+                bot.keyPress(KeyEvent.VK_A);
+                bot.keyRelease(KeyEvent.VK_A);
+                break;
+            case "b":
+                bot.keyPress(KeyEvent.VK_B);
+                bot.keyRelease(KeyEvent.VK_B);
+                break;
+            case "c":
+                bot.keyPress(KeyEvent.VK_C);
+                bot.keyRelease(KeyEvent.VK_C);
+                break;
+            case "d":
+                bot.keyPress(KeyEvent.VK_D);
+                bot.keyRelease(KeyEvent.VK_D);
+                break;
+            case "e":
+                bot.keyPress(KeyEvent.VK_E);
+                bot.keyRelease(KeyEvent.VK_E);
+                break;
+            case "f":
+                bot.keyPress(KeyEvent.VK_F);
+                bot.keyRelease(KeyEvent.VK_F);
+                break;
+            case "g":
+                bot.keyPress(KeyEvent.VK_G);
+                bot.keyRelease(KeyEvent.VK_G);
+                break;
+            case "h":
+                bot.keyPress(KeyEvent.VK_H);
+                bot.keyRelease(KeyEvent.VK_H);
+                break;
+            case "i":
+                bot.keyPress(KeyEvent.VK_I);
+                bot.keyRelease(KeyEvent.VK_I);
+                break;
+            case "j":
+                bot.keyPress(KeyEvent.VK_J);
+                bot.keyRelease(KeyEvent.VK_J);
+                break;
+            case "k":
+                bot.keyPress(KeyEvent.VK_K);
+                bot.keyRelease(KeyEvent.VK_K);
+                break;
+            case "l":
+                bot.keyPress(KeyEvent.VK_L);
+                bot.keyRelease(KeyEvent.VK_L);
+                break;
+            case "m":
+                bot.keyPress(KeyEvent.VK_M);
+                bot.keyRelease(KeyEvent.VK_M);
+                break;
+            case "n":
+                bot.keyPress(KeyEvent.VK_N);
+                bot.keyRelease(KeyEvent.VK_N);
+                break;
+            case "o":
+                bot.keyPress(KeyEvent.VK_O);
+                bot.keyRelease(KeyEvent.VK_O);
+                break;
+            case "p":
+                bot.keyPress(KeyEvent.VK_P);
+                bot.keyRelease(KeyEvent.VK_P);
+                break;
+            case "q":
+                bot.keyPress(KeyEvent.VK_Q);
+                bot.keyRelease(KeyEvent.VK_Q);
+                break;
+            case "r":
+                bot.keyPress(KeyEvent.VK_R);
+                bot.keyRelease(KeyEvent.VK_R);
+                break;
+            case "s":
+                bot.keyPress(KeyEvent.VK_S);
+                bot.keyRelease(KeyEvent.VK_S);
+                break;
+            case "t":
+                bot.keyPress(KeyEvent.VK_T);
+                bot.keyRelease(KeyEvent.VK_T);
+                break;
+            case "u":
+                bot.keyPress(KeyEvent.VK_U);
+                bot.keyRelease(KeyEvent.VK_U);
+                break;
+            case "v":
+                bot.keyPress(KeyEvent.VK_V);
+                bot.keyRelease(KeyEvent.VK_V);
+                break;
+            case "w":
+                bot.keyPress(KeyEvent.VK_W);
+                bot.keyRelease(KeyEvent.VK_W);
+                break;
+            case "x":
+                bot.keyPress(KeyEvent.VK_X);
+                bot.keyRelease(KeyEvent.VK_X);
+                break;
+            case "y":
+                bot.keyPress(KeyEvent.VK_Y);
+                bot.keyRelease(KeyEvent.VK_Y);
+                break;
+            case "z":
+                bot.keyPress(KeyEvent.VK_Z);
+                bot.keyRelease(KeyEvent.VK_Z);
+                break;
+            case "A":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_A);
+                bot.keyRelease(KeyEvent.VK_A);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "B":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_B);
+                bot.keyRelease(KeyEvent.VK_B);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "C":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_C);
+                bot.keyRelease(KeyEvent.VK_C);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "D":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_D);
+                bot.keyRelease(KeyEvent.VK_D);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "E":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_E);
+                bot.keyRelease(KeyEvent.VK_E);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "F":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_F);
+                bot.keyRelease(KeyEvent.VK_F);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "G":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_G);
+                bot.keyRelease(KeyEvent.VK_G);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "H":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_H);
+                bot.keyRelease(KeyEvent.VK_H);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "I":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_I);
+                bot.keyRelease(KeyEvent.VK_I);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "J":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_J);
+                bot.keyRelease(KeyEvent.VK_J);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "K":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_K);
+                bot.keyRelease(KeyEvent.VK_K);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "L":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_L);
+                bot.keyRelease(KeyEvent.VK_L);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "M":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_M);
+                bot.keyRelease(KeyEvent.VK_M);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "N":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_N);
+                bot.keyRelease(KeyEvent.VK_N);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "O":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_O);
+                bot.keyRelease(KeyEvent.VK_O);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "P":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_P);
+                bot.keyRelease(KeyEvent.VK_P);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "Q":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_Q);
+                bot.keyRelease(KeyEvent.VK_Q);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "R":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_R);
+                bot.keyRelease(KeyEvent.VK_R);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "S":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_S);
+                bot.keyRelease(KeyEvent.VK_S);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "T":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_T);
+                bot.keyRelease(KeyEvent.VK_T);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "U":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_U);
+                bot.keyRelease(KeyEvent.VK_U);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "V":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_V);
+                bot.keyRelease(KeyEvent.VK_V);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "W":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_W);
+                bot.keyRelease(KeyEvent.VK_W);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "X":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_X);
+                bot.keyRelease(KeyEvent.VK_X);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "Y":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_Y);
+                bot.keyRelease(KeyEvent.VK_Y);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+            case "Z":
+                bot.keyPress(KeyEvent.VK_SHIFT);
+                bot.keyPress(KeyEvent.VK_Z);
+                bot.keyRelease(KeyEvent.VK_Z);
+                bot.keyRelease(KeyEvent.VK_SHIFT);
+                break;
+
+            //"enter,space,backspace,up,down,left,right,escape";
+
+            case "enter":
+                bot.keyPress(KeyEvent.VK_ENTER);
+                bot.keyRelease(KeyEvent.VK_ENTER);
+                break;
+            case "space":
+                bot.keyPress(KeyEvent.VK_SPACE);
+                bot.keyRelease(KeyEvent.VK_SPACE);
+                break;
+            case "backspace":
+                bot.keyPress(KeyEvent.VK_BACK_SPACE);
+                bot.keyRelease(KeyEvent.VK_BACK_SPACE);
+                break;
+            case "up":
+                bot.keyPress(KeyEvent.VK_UP);
+                bot.keyRelease(KeyEvent.VK_UP);
+                break;
+            case "down":
+                bot.keyPress(KeyEvent.VK_DOWN);
+                bot.keyRelease(KeyEvent.VK_DOWN);
+                break;
+            case "left":
+                bot.keyPress(KeyEvent.VK_LEFT);
+                bot.keyRelease(KeyEvent.VK_LEFT);
+                break;
+            case "right":
+                bot.keyPress(KeyEvent.VK_RIGHT);
+                bot.keyRelease(KeyEvent.VK_RIGHT);
+                break;
+            case "escape":
+                bot.keyPress(KeyEvent.VK_ESCAPE);
+                bot.keyRelease(KeyEvent.VK_ESCAPE);
+                break;
+            default:
+                System.out.println("error with press(): invalid key: " + key);
+                break;
+        }
     }
 
     public static void highlightErrorLine(int lineNumber, String[] lines, TextArea textArea, String[] scriptLine, int lengthDifference, int lengthDifferenceSingleLine) {
@@ -429,7 +773,7 @@ public class Main extends Application {
                                     Set<String> keySet = Set.of(keysArray);
 
                                     if (keySet.contains(scriptLine[1])) {
-                                        System.out.println("this is a valid key");
+                                        //System.out.println("this is a valid key");
                                     } else {
 
                                         System.out.print("invalid key for press command. arg: " + scriptLine[1]);
@@ -815,8 +1159,9 @@ public class Main extends Application {
                                         Set<String> keySet = Set.of(keysArray);
 
                                         if (keySet.contains(scriptLine[1])) {
-                                            System.out.println("this is a valid key");
-                                            System.out.println("press feature is not yet implemented! implement it here!!!!!");
+                                            //System.out.println("this is a valid key");
+                                            //System.out.println("press feature is not yet implemented! implement it here!!!!!");
+                                            press(scriptLine[1], bot);
                                         } else {
 
                                             System.out.print("invalid key for press command. arg: " + scriptLine[1]);
@@ -1132,8 +1477,8 @@ public class Main extends Application {
         MenuItem aboutItem1 = new MenuItem("About");
         Alert aboutAlert = new Alert(Alert.AlertType.INFORMATION);
         aboutAlert.setTitle("About");
-        aboutAlert.setHeaderText("About AutoInput v0.0040");
-        aboutAlert.setContentText("This is an input automation scripting language and editor made by 0x416c616e. You can use it to write keyboard/mouse macros" +
+        aboutAlert.setHeaderText("About AutoInput v0.0041");
+        aboutAlert.setContentText("This is an input automation scripting language and editor made by 0x416c616e (Alan). You can use it to write keyboard/mouse macros" +
                 " in order to automate repetitive tasks that require using a GUI rather than something command line-based that can be automated with a shell script.");
 
 
